ec51d8eaa26efc212af6b98e9f1d105c19818cb2
Vishal Soni <vs332837@gmail.com>
2023-02-25 21:03:58 +0530

    update

---
+++ Basic Projects/draggable image slider in html,css,js/test.js
@@ -0,0 +1,9 @@
+const mousemove = (e) =>{
+    // console.log(e);
+    // console.log("x = "+e.clientX);
+    // console.log("y = "+e.clientY);
+    console.log("pagex = "+e.pageX);
+    
+    
+}
+window.addEventListener(`mousemove`,mousemove);
