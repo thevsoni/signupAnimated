55925269c20705ac629a812801a02c8e0a02adec
Vishal Soni <vs332837@gmail.com>
2023-03-02 13:11:38 +0530

    update

---
+++ Basic Projects/login signup forms/login form validation with shake effects using html,css,js/test.js
@@ -0,0 +1,11 @@
+const form = document.querySelector("form");
+
+const emailInputBox = form.querySelector(".email")
+const emailInput = emailInputBox.querySelector("input")
+
+const pwdInputBox = form.querySelector(".password")
+const pwdInput = pwdInputBox.querySelector("input")
+
+form.onsubmit = () => {
+
+}
