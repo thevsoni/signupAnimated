76183a04434c109133eee27d62c76151ec3861ed
Vishal Soni <vs332837@gmail.com>
2023-04-15 16:27:28 +0530

    update

---
+++ viedo-streaming/netflix/client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='h-header bg-main w-full'>
+      <h1 className='text-h1 text-subMain font font-bold underline'>Hello world</h1>
+    </div>
+  )
+}
+
+export default App
